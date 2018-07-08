76f174ea027314e945e8f84b2a9c23fbd32834cc
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2018-07-08 23:15:48 +0200

    Basic command line interface for GET requests to given address
    Signed-off-by: Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,56 @@
+extern crate hyper;
+extern crate futures;
+extern crate hyper_tls;
+
+use hyper::Response;
+use hyper::Method;
+use hyper::Client;
+use hyper::rt::{self, Future, Stream};
+use hyper::HeaderMap;
+use hyper::Uri;
+use std::io::{self, Write};
+use hyper_tls::HttpsConnector;
+
+pub fn request_uri(addr: Uri, method: Method) -> RequestResult<()> {
+    let https_connector = HttpsConnector::new(4).unwrap();
+    let client = Client::builder()
+        .keep_alive(false)
+        .build::<_, hyper::Body>(https_connector);
+
+    let fut = client
+        .get(addr)
+        .and_then(|res| {
+            for (key, value) in res.headers() {
+                println!("{}: {}", key.as_str(), value.to_str().unwrap());
+            }
+            res.into_body().concat2()
+        })
+        .and_then(|body| {
+            let s = ::std::str::from_utf8(&body)
+                .expect("Reponse must by valid utf-8");
+            println!("---");
+            println!("{}", s);
+            Ok(())
+        })
+        .map_err(|err| {
+            println!("error: {}", err);
+        });
+
+    rt::run(fut);
+    Ok(())
+}
+
+pub struct ResponseComponents {
+    headers: HeaderMap,
+}
+
+pub type RequestResult<T> = Result<T, RequestError>;
+
+#[derive(Debug)]
+pub enum RequestError {
+    Hyper(hyper::Error)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate rust_client;
+extern crate clap;
+extern crate hyper;
+
+use clap::{Arg, App};
+
+use rust_client::request_uri;
+use hyper::Uri;
+use hyper::Method;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rust-client")
+        .version("0.1.0")
+        .author("Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>")
+        .about("A command line http client")
+        .arg(Arg::with_name("URI")
+            .help("The URI to send the request to")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+    let target = matches.value_of("URI").expect("URI is required!");
+
+    println!("GET {}", target);
+    request_uri(target.parse::<Uri>().unwrap(), Method::HEAD);
 }
