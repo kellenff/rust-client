25eca01c805a3ea87136d9ace6ee165c2205e32e
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2018-11-20 16:48:06 +0100

    Update dependencies and clean up integration test
    Signed-off-by: Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>

--- tests/verbs.rs
+++ tests/verbs.rs
@@ -39,20 +39,3 @@ fn get_simple() {
     assert_eq!(response.status(), StatusCode::OK);
     assert_eq!(response.text(), GET_RESP_BODY);
 }
-//
-//#[test]
-//fn head_simple() {
-//    let _m = mock("HEAD", "/")
-//        .with_status(200)
-//        .with_header("content-type", "text/plain")
-//        .create();
-//
-//    let matches = cli_app().get_matches_from(&["rc", "head", TARGET_URL]);
-//    let config = RunConfig::from(matches);
-//
-//    assert_eq!(config.method(), Method::HEAD);
-//
-//    let response = config.execute().unwrap().response;
-//
-//    assert_eq!(response.status(), StatusCode::OK);
-//}
